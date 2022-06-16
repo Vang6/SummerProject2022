6b4d75ec778886d24fde14860bce6f8c821717ae
Dev Aggarwal <vasudevaggarwal1@vasudevs-MacBook-Air.local>
2022-06-16 13:01:45 +0530

    commit 02

--- issue_management_tool/src/screens/registration.screen.js
+++ issue_management_tool/src/screens/registration.screen.js
@@ -2,7 +2,32 @@ import React from 'react';
 const Registration= function(){
     return(
     <div>
-     Registration Page
+      <div>
+             <div className="login-form">
+            <form>
+                <div className="form-group">
+                    <h2>Login</h2>
+                    </div>
+                <div className="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
+                        else.</small>
+                </div>
+                <div className="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                </div>
+                <div className="form-group form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                    <label className="form-check-label" for="exampleCheck1">Technician</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <hr/>
+            </form>
+        </div>
+
+    </div>
     </div> );
 };
 export default Registration;
